8b9a2deff332b0a40e5b9ac6245793edeed1b933
Alex.Daemen <alex.daemen@ns.nl>
2024-12-09 16:00:57 +0100

    create tests and code to accept conditions with 0- and 1 outputs

--- garden-raker/src/lib/playground.test.ts
+++ garden-raker/src/lib/playground.test.ts
@@ -2,8 +2,18 @@ import { it, expect } from 'vitest';
 import { exampleFunction } from './playground';
 
 describe('solutions', () => {
-  it('tests my first function', () => {
-    const input = 0;
+  it('returns 0 when garden string is empty', () => {
+    const input = '';
     expect(exampleFunction(input)).toBe(0);
+  });
+
+  it('returns gravel when 1 value input is given that is not rock or gravel', () => {
+    const input = 'slug';
+    expect(exampleFunction(input)).toBe('gravel');
+  });
+
+  it('does not change output to gravel if input is rock', () => {
+    const input = 'rock';
+    expect(exampleFunction(input)).toBe('rock');
   });
 })
--- garden-raker/src/lib/playground.ts
+++ garden-raker/src/lib/playground.ts
@@ -1,3 +1,4 @@
-export const exampleFunction = (value) => {
-  return value;
+export const exampleFunction = (value: string) => {
+  const rake = !value.includes('gravel') && !value.includes('rock') ? value.replace(value, 'gravel') : value;
+  return value.length === 0 ? 0 : rake;
 };
