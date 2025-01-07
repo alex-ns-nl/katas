10e83755c2ccf21be7feb98e3c3e5977b90004d3
Alex.Daemen <alex.daemen@ns.nl>
2025-01-07 11:40:18 +0100

    Refactor

--- booty-escape/src/lib/playground.test.ts
+++ booty-escape/src/lib/playground.test.ts
@@ -1,50 +1,33 @@
 import { it, expect } from 'vitest';
 import { isRouteSafe } from './playground';
 
-describe('solutions', () => {
-  it('can evaluate safe routes', () => {
-    const map = [['X']];
-
-    const routeSafety = isRouteSafe(map);
-
-    expect(routeSafety).toBe(true);
-  });
-
-  it('can evaluate not safe routes', () => {
-    const map = [['X', 'N']];
-
-    const routeSafety = isRouteSafe(map);
-
-    expect(routeSafety).toBe(false);
-  })
-})
-
-
-const maps = [['X']];
-
-// describe('Can evaluate safe routes', () => {
-//   test.each(maps)('%s', (map: string[][]) => {
-//     const routeSafety = isRouteSafe(map);
-//
-//     expect(routeSafety).toBe(true);
-//
-//     // console.log(`Running test for: ${value}`);
-//   });
-// });
-
-
 describe('Map Tests', () => {
   const safeMaps = [
-    { map: [['X']] },
+    {map: [['X']]},
   ];
 
-  safeMaps.forEach(({ map }) => {
+  const unsafeMaps = [
+    {map: [['X', 'N']]},
+  ];
+
+  safeMaps.forEach(({map}) => {
     const description = `map with values ${JSON.stringify(map)}`;
     describe(`Testing routes: ${description}`, () => {
       test('should test whether route is safe', () => {
         const routeSafety = isRouteSafe(map);
 
         expect(routeSafety).toBe(true);
+      });
+    });
+  });
+
+  unsafeMaps.forEach(({map}) => {
+    const description = `map with values ${JSON.stringify(map)}`;
+    describe(`Testing unsafe routes: ${description}`, () => {
+      test('should test whether route is not safe', () => {
+        const routeSafety = isRouteSafe(map);
+
+        expect(routeSafety).toBe(false);
       });
     });
   });
