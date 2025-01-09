24830a810b5d182f7adf92bbd740755699c99596
Alex.Daemen <alex.daemen@ns.nl>
2025-01-09 09:33:12 +0100

    R

--- cimbingStairs/src/lib/playground.ts
+++ cimbingStairs/src/lib/playground.ts
@@ -1,5 +1,6 @@
 export const exampleFunction = (stairArray: number[][]) => {
-  let totalStairs = 0
+  let totalStairs = 0;
+  
   stairArray.forEach(
     weekday => weekday.forEach(stairNumber => totalStairs += stairNumber * 20)
   );
