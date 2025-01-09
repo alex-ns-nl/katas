a9f27929454555443edbaa970e016d867d6ea9a6
Alex.Daemen <alex.daemen@ns.nl>
2025-01-09 09:27:47 +0100

    R

--- cimbingStairs/src/lib/playground.ts
+++ cimbingStairs/src/lib/playground.ts
@@ -1,14 +1,8 @@
-export const exampleFunction = (value: number[][]) => {
-  console.log('value: ', value.length);
-  let total = 0
-  value.forEach(weekday => {    
-    if (weekday.length === 0) {
-      total += 0;
-    } 
-    else if (weekday.length > 0){
-      weekday.forEach(number => total += number * 20);
-    }
+export const exampleFunction = (stairArray: number[][]) => {
+  let totalStairs = 0
+  stairArray.forEach(weekday => {    
+    weekday.forEach(stairNumber => totalStairs += stairNumber * 20);
   });
   
-  return total;
+  return totalStairs;
 };
